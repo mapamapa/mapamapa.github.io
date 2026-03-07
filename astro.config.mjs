36a27d3efe3c353bb7f08cee2579e8da02f84f78
srcstg <david@d10.cl>
2026-03-06 21:15:39 -0300

    git dir

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://mapamapa.cl',
+  site: 'https://mapamapa.github.io',
   output: 'static',
   integrations: [sitemap()],
   build: {
